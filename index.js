3ff606ea4285f7e81b17f9d2e50f61adf6962e01
jorgezambra <jorgezambra@gmail.com>
2021-02-26 18:53:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 const bodyParser = require('body-parser');
 const uuid = require('uuid');
@@ -47,6 +48,12 @@ let auth = require('./auth')(app);
 app.use(morgan('common'));
 
 app.use(express.static('public'));
+
+app.use("/client", express.static(path.join(__dirname, "client", "dist")));
+
+app.get("/client/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(cors());
 
